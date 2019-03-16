fa2f21e5b1ded11a29a671e864d4a84fd04c0033
miniyoung37 <miniyoung37@gmail.com>
2019-03-16 23:58:58 +0900

    remove 주석

--- src/EntryPoint.java
+++ src/EntryPoint.java
@@ -5,11 +5,6 @@ public class EntryPoint {
 
     public static void main(String[] args) {
 
-//        UserModel mini = new UserModel("mini", "0909");
-//
-//        System.out.println("[ USER MODEL ]");
-//        System.out.println(mini.toString());
-
         LoginView loginView = new LoginView();
         loginView.registerView();
 
