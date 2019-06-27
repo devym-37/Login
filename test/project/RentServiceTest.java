d9f471dc970736a5185b3ef6248874628e40c14b
miniyoung37 <miniyoung37@gmail.com>
2019-06-28 00:12:40 +0900

    Service Test Junit complete.

--- test/project/RentServiceTest.java
+++ test/project/RentServiceTest.java
@@ -1,20 +1,33 @@
 package project;
 
 import DB.LocalDB;
-import model.UserModel;
+import model.RentModel;
 import org.junit.Before;
 import org.junit.Test;
-import repository.UserRepository;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import repository.BookRepository;
+import repository.RentRepository;
 import service.RentService;
-import service.UserService;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 
+@RunWith(MockitoJUnitRunner.class)
 public class RentServiceTest {
 
+    @Mock
+    RentRepository rentRepository;
+    BookRepository bookRepository;
+
+    @InjectMocks
     RentService rentService = new RentService();
-    UserService userService = new UserService();
 
     @Before
     public void setUp(){
@@ -22,14 +35,77 @@ public class RentServiceTest {
     }
 
     @Test
-    public void rentBookTest(){
+    public void saveRent_return1(){
         //  given
-        UserRepository mockUserRepository = mock(UserRepository.class);
-        UserModel currentUserModel = new UserModel("thor2", "password", "name", "email@company.com");
+        String id = "temp";
+        String writer = "tempWriter";
+        String bookName = "tempBookname";
+        String amount = "1";
+        String isbn = "999999999";
 
-        given(mockUserRepository.currentUser()).willReturn(currentUserModel);
+        RentModel rentModel = new RentModel();
+        rentModel.setId(id);
+        rentModel.setWriter(writer);
+        rentModel.setBookName(bookName);
+        rentModel.setAmount(amount);
+        rentModel.setIsbn(isbn);
+
+        List<RentModel> rentModelList = new ArrayList<>();
+        rentModelList.add(rentModel);
+
+        given(rentRepository.findrent()).willReturn(rentModelList);
+
         //  when
-        rentService.rentBook();
+        int result = rentService.saveRent(id, writer, bookName, amount, isbn);
+
         //  then
+        assertThat(result, is(1));
     }
+
+    @Test
+    public void rentValue_return0(){
+        //  given
+        String value = "temp";
+
+        //  when
+        int result = rentService.rentValue(value);
+
+        //  then
+        assertThat(result, is(0));
+    }
+
+    @Test
+    public void rentValue_return1(){
+        //  given
+        String value = "Y";
+
+        //  when
+        int result = rentService.rentValue(value);
+
+        //  then
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void rentValue_return2(){
+        //  given
+        String value = "N";
+
+        //  when
+        int result = rentService.rentValue(value);
+
+        //  then
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void rentBookTest(){
+
+    }
+
+    @Test
+    public void returnBookTest(){
+
+    }
+
 }
